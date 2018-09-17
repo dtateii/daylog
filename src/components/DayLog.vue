d30babc0e70403cce22253c7ea15b2f96cf0b7ce
Dave Tate <dtate@njimedia.com>
2018-09-17 11:41:45 -0400

    Style: Improve daylog header styles
    Flex for vertical positioning and dividers, font weights.
    Add color highlighting for 'today'

--- src/components/DayLog.vue
+++ src/components/DayLog.vue
@@ -1,29 +1,37 @@
 <template>
   <div class="daylog">
-    <h2>{{title}}</h2>
+    <h2>{{monthName}}</h2>
     <div
       class="day"
       v-for="(logDay, key) in logDays"
-      v-bind:key="key">
+      v-bind:key="key"
+      v-bind:class="{today: logDay.isToday}">
       <header>
-        <h3 class="dayNum">{{key + 1}}</h3>
-        <span class="dayName">{{logDay.name}}</span>
+        <div class="divider">
+          <span class="monthLabel">{{monthName}}</span>
+        </div>
+        <div class="dayDetails">
+          <h3 class="dayNum">{{key + 1}}</h3>
+          <span class="dayName">{{logDay.name}}</span>
+        </div>
       </header>
-      <div
-      class="row"
-      v-for="logEntry in logDay.entries"
-      v-bind:key="logEntry.id">
-        <span class="time">{{logEntry.timestamp | timeHuman}}</span>
-        <input
-          class="description"
-          :value="logEntry.activity"
-          @keyup.enter="insertLog"
-          @change="updateEntry(logEntry.id, $event.target.value)"
-          />
-          <!-- <button
-        class="insertion"
-        @click.prevent="newRow">+</button> -->
-      </div>
+      <section class="entries">
+        <div
+        class="row"
+        v-for="logEntry in logDay.entries"
+        v-bind:key="logEntry.id">
+          <span class="time">{{logEntry.timestamp | timeHuman}}</span>
+          <input
+            class="description"
+            :value="logEntry.activity"
+            @keyup.enter="insertLog"
+            @change="updateEntry(logEntry.id, $event.target.value)"
+            />
+            <!-- <button
+          class="insertion"
+          @click.prevent="newRow">+</button> -->
+        </div>
+      </section>
     </div>
     <div class="row">
       <ClockEntry />
@@ -49,8 +57,7 @@ export default {
     })
   },
   computed: {
-    title: function () {
-      // Use the Month name as the title
+    monthName: function () {
       // logId has format YYYY.MM
       let date = new Date(this.logId)
       return date.toLocaleString('en-US', {month: 'long'})
@@ -63,13 +70,15 @@ export default {
       // todo: This should conditionally limit either: num days in month, or,
       // if month-in-view is current month, limit to  num days so far in month.
       // In development, hard-coding limit to current day in current month.
-      let dayLimit = dayNumToday
+      let dayLimit = dayNumToday // todo
+      let currentMonth = true // todo
       // Start day numeration from first day in this month.
       let weekdayNum = firstDateOfMonth.getDay()
       for (let i = 0; i < dayLimit; i++) {
         days[i] = {
           name: TimeHelper.getWeekdayName(weekdayNum),
-          entries: []
+          entries: [],
+          isToday: currentMonth && i === (dayNumToday - 1)
         }
         // iterate weekday number.
         if (weekdayNum === 6) {
@@ -128,15 +137,45 @@ export default {
   & header {
     color: white;
   }
+  .divider {
+    opacity: 0.5;
+    display: flex;
+  }
+  .divider:after {
+    content: "";
+    border-bottom: 1px solid;
+    margin-left: 0.5em;
+    width: 100%;
+    height: 0.5em;
+  }
+  .monthLabel {
+    font-size: 0.7em;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+  .dayDetails {
+    display: flex;
+    align-items: center;
+    .dayNum {
+      display: inline-block;
+      margin: 0 0.4em 0 0;
+      font-size: 2em;
+      font-weight: 100;
+    }
+    .dayName {
+      text-transform: uppercase;
+      font-weight: 100;
+    }
+  }
+  &.today .dayDetails .dayNum,
+  &.today .dayDetails .dayName {
+    color: #ff6070;
+  }
+  .entries {
+    margin-bottom: 2em;
+  }
 }
-.dayNum {
-  display: inline-block;
-  margin-right: 0.4em;
-  font-size: 2em;
-}
-.dayName {
-  text-transform: uppercase;
-}
+
 .time {
     text-align: right;
     display: inline-block;
